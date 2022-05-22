2edf30650a34f458204fe4eb48778d82e2329cc9
tshefu <schneider.viktoria95@gmail.com>
2022-05-22 18:22:41 +0200

    added link to the github repository

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
     <div className="App">
       <h1>Local Weather</h1>
       <SearchForm />
+      <p>
+        This project was coded by Viktoria Schneider and is open-sourced on
+        <a
+          href="https://github.com/tshefu/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
     </div>
   );
 }
